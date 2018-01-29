374e3388f9f042a7641aae69d867b6f22f645557
Ruairi Oliver <ruairioliver@corehr.com>
2018-01-29 17:13:21 +0000

    a

--- ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/AHBottomNavigation.java
+++ ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/AHBottomNavigation.java
@@ -552,7 +552,7 @@ public class AHBottomNavigation extends FrameLayout {
 			}
 
 			icon.setImageDrawable(AHHelper.getTintDrawable(items.get(i).getDrawable(context),
-					Color.BLUE, forceTint));
+					i == 2 ? Color.BLUE : itemInactiveColor, forceTint));
 			title.setTextColor(currentItem == i ? itemActiveColor : itemInactiveColor);
 			title.setAlpha(currentItem == i ? 1 : 0);
 			view.setOnClickListener(new OnClickListener() {
